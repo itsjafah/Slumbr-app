2b9b7e83fac4452aec1bed50a3e428a081d6eed0
github username <github email address>
2018-10-24 07:24:30 -0400

    added scrolling ability to the menu buttons

--- slumber-frontend/src/index.js
+++ slumber-frontend/src/index.js
@@ -10,6 +10,19 @@ let flkty = new Flickity( elem, {
   wrapAround: true,
   autoPlay: 3000,
   pauseAutoPlayOnHover: false
-});
+})
 
 let instance = M.Tabs.getInstance(elem)
+
+window.addEventListener("click", function (event) {
+  if (event.target.innerText === "HOW IT WORKS") {
+    window.scrollTo({top: 640, behavior: 'smooth'})
+    event.target.className = "active"
+  } else if (event.target.innerText === "SLUMBR") {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+    event.target.className = "active"
+  } else if (event.target.innerText === "ABOUT US") {
+    window.scrollTo({top: 1200, behavior: 'smooth'})
+    event.target.className = "active"
+  }
+})
